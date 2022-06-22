35d57d462ef1b613e72eae43dc40d5b10e43a813
jpcp0614 <jpcp0614@gmail.com>
2022-06-22 17:37:00 -0300

    numero aleatorio

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Primeiro from './components/basics/Primeiro';
 import ComParametro from './components/basics/ComParametro';
+import RandomNumber from './components/basics/Aleatorio';
 
 const App = () => (
 	<>
 		<h1>Fundamentos React</h1>
 		<Primeiro />
 		<ComParametro titulo='Situação do aluno' aluno='João' nota={7.9} />
+    <RandomNumber min={ 1 } max={ 20 } />
 	</>
 );
 
