3d96feb887d4bc5b7e4cf41468a2781b59f1fce4
jpcp0614 <jpcp0614@gmail.com>
2022-06-22 18:24:35 -0300

    Componente card 02 - css

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -3,26 +3,25 @@ import Primeiro from './components/basics/Primeiro';
 import ComParametro from './components/basics/ComParametro';
 import RandomNumber from './components/basics/Aleatorio';
 import Card from './components/layout/Card';
+import './App.css';
 
 const App = () => (
-	<>
+	<div className="App">
 		<h1>Fundamentos React</h1>
+		<div className="Cards">
+			<Card titulo="Desafio Aleatório">
+				<RandomNumber min={ 1 } max={ 20 } />
+			</Card>
 
-		<Card titulo="Desafio Aleatório">
-			<RandomNumber min={ 1 } max={ 20 } />
-		</Card>
+			<Card titulo="Primeiro">
+				<Primeiro />
+			</Card>
 
-		<Card titulo="Primeiro">
-			<Primeiro />
-		</Card>
-
-		<Card titulo="Com Parâmetro">
-			<ComParametro titulo='Situação do aluno' aluno='João' nota={7.9} />
-		</Card>
-		
-		
-    
-	</>
+			<Card titulo="Com Parâmetro">
+				<ComParametro titulo='Situação do aluno' aluno='João' nota={7.9} />
+			</Card>
+		</div>
+	</div>
 );
 
 export default App;
