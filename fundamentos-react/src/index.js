0b1091b9923f794cdc81d0a208c3a7b2c3fc9063
jpcp0614 <jpcp0614@gmail.com>
2022-06-22 15:30:33 -0300

    Hello World

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,7 @@
+import ReactDom from 'react-dom';
+
+const el = document.getElementById('root');
+
+ReactDom.render('Hello World!!!', el);
+
+//ReactDom.render('Olá React!!!', document.getElementById('root'));
