4f5267f1f6d1ec3e3bb9ceba116bb3e46cb25e2f
jpcp0614 <jpcp0614@gmail.com>
2022-06-22 15:35:51 -0300

    conhecendo jsx

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,7 +1,14 @@
 import ReactDom from 'react-dom';
+import React from 'react';
 
 const el = document.getElementById('root');
 
-ReactDom.render('Hello World!!!', el);
+const tag = <strong>Hello World!!!</strong>
+
+ReactDom.render(
+  <div>
+    { tag }
+  </div>,
+el);
 
 //ReactDom.render('Olá React!!!', document.getElementById('root'));
