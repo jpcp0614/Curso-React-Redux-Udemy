636348c6b935b2e3a948dd80e3174e81a508da80
jpcp0614 <jpcp0614@gmail.com>
2022-06-23 19:39:30 -0300

    :sparkles: feat: className conditional

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -3,8 +3,16 @@ import './Button.css';
 
 const Button = (props) => {
   return (
-    <button className="button">
-      {props.label}
+    <button
+      className={`
+        button
+        ${props.operation ? 'operation' : ''}
+        ${props.double ? 'double' : ''}
+        ${props.triple ? 'triple' : ''}
+      `}
+      onClick={() => props.click && props.click(props.label)}
+      >
+      { props.label }
     </button>
   )
 }
