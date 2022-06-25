2b19477f75524bb854861e0c7eccfcc327466d84
jpcp0614 <jpcp0614@gmail.com>
2022-06-25 13:27:12 -0300

    NotFound

--- navegacao/src/components/layout/Content.jsx
+++ navegacao/src/components/layout/Content.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import About from '../../views/examples/About';
 import Home from '../../views/examples/Home';
+import NotFound from '../../views/examples/NotFound';
 import Param from '../../views/examples/Param';
 import './Content.css';
 
@@ -12,6 +13,7 @@ const Content = (props) => {
         <Route path="/" element={ <Home /> } />
         <Route path="/about" element={ <About /> }/>
         <Route path="/param/:id" element={ <Param /> }/>
+        <Route path="*" element={ <NotFound /> }/>
       </Routes>
     </main>
   );
