bf8a3b76d1d66aa8a2d02555394e098d599a1f4c
jpcp0614 <jpcp0614@gmail.com>
2022-06-25 00:27:45 -0300

    Configurações iniciais

---
+++ navegacao/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createRoot } from 'react-dom';
+
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <h1>App</h1>
+  </div>
+);
+
